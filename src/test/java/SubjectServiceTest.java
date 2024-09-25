bfd0752d2f9e8c21a7ce48e80ae50ad0ac1faf87
Samuel Dobrík <56543331+phosp4@users.noreply.github.com>
2024-09-26 00:01:03 +0200

    Test SubjectService with unit testing

--- src/test/java/SubjectServiceTest.java
+++ src/test/java/SubjectServiceTest.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -6,10 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SubjectServiceTest {
 
+    private SubjectService ss;
+    @BeforeEach
+    void setUp() {
+        ss = new SubjectService();
+    }
 
     @org.junit.jupiter.api.Test
     void getAttendeesCount_empty() {
-        SubjectService ss = new SubjectService();
 
         Set<Student> students = new HashSet<>();
 
@@ -19,5 +26,27 @@ class SubjectServiceTest {
         subject.setAttendees(students);
 
         assertEquals(0, ss.getAttendeesCount(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSubjectById_empty() {
+
+        Subject s = new Subject();
+        assertNull(ss.getSubjectById(123));
+    }
+
+    @Test
+    void getAttendeesCount_happypath() {
+
+        Set<Student> students = new HashSet<>();
+        students.add(new Student());
+
+        Subject subject = new Subject();
+        subject.setId(1);
+        subject.setAttendees(students);
+
+        ss.addSubject(subject);
+
+        assertEquals(1, ss.getAttendeesCount(1));
     }
 }
