56052f5f142b78c5b93c44f7b9584f5d21f4fea3
Samuel Dobrík <56543331+phosp4@users.noreply.github.com>
2024-09-26 00:08:29 +0200

    simplify the code in SubjectService class

--- src/main/java/SubjectService.java
+++ src/main/java/SubjectService.java
@@ -2,31 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SubjectService {
-    private final List<Subject> subjectList;
-
-    public SubjectService() {
-        subjectList = new ArrayList<>();
-    }
-
-    public void addSubject(Subject subject) {
-        subjectList.add(subject);
-    }
-
-    public Subject getSubjectById(long id) {
-        for (Subject subject : subjectList) {
-            if (subject.getId() == id) return subject;
-            }
-
-        return null;
-    }
-
-    public int getAttendeesCount(long subjectId) {
-        Subject subject = getSubjectById(subjectId);
-
+    public int getAttendeesCount(Subject subject) {
         if (subject == null) return 0;
-
         return subject.getAttendees().size();
     }
-
-
 }
